74eb797c7b85b93d09c665085472c73e1c6b10d2
James Scott <james.scott@mccombs.utexas.edu>
2017-01-28 15:18:50 -0600

    exercises 2

--- exercises/README.md
+++ exercises/README.md
@@ -10,3 +10,7 @@ The goal of [Exercises 1](exercises01-SDS383D.pdf) is to provide you with a revi
 
 It will also treat the multivariate normal distribution in (possibly excruciating) depth.  But since this distribution is so fundamental to working with hierarchical Bayesian models, it's worth the effort.  
 
+## Exercises 2: Bayes and the Gaussian linear model  
+
+The goal of [Exercises 2](exercises02-SDS383D.pdf) is to get you comfortable with the use of conjugate priors in Gaussian models.  It finishes with a cool use of hierarchical modeling to fit a regression model with heavy-tailed errors.  
+
--- r/simple_gibbs.R
+++ r/simple_gibbs.R
@@ -1,38 +1,38 @@
 theta_true = 10
 sig_true = 3.5
-N = 50
+N = 75
 
 y = rnorm(N, theta_true, sig_true)
 ybar = mean(y)
+Sy = sum((y - ybar)^2)
 
+# Prior for 1/sigma2 ~ InvGamma(d/2, eta2/2)
+d = 1
+eta2 = 1
 
-# A simple Gibbs sampler
+# Prior for theta: super vague N(mu, tau2)
+mu_theta = 0
+tau2_theta = 10^2
+gamma_theta = 1/tau2_theta
 
-# Prior for 1/sigma2
-a = 1
-b = 1
-
-# Prior for theta: super vague
-m_theta = 0
-v_theta = 100^2
+### Method 1: a simple Gibbs sampler
 
 # Number of Monte Carlo draws
-NMC = 1000
+NMC = 5000
 theta_save = rep(0, NMC)
 sigma2_save = rep(0, NMC)
 
 # Initial state
 sigma2 = 1
-
 for(i in 1:NMC) {
 	
 	# Conjugate update for theta, given sigma2
-	theta_var = 1/{N/sigma2 + 1/v_theta}  # sum of precisions
-	theta_hat = theta_var * {(N/sigma2)*ybar + m_theta/v_theta}
+	theta_var = 1/{N/sigma2 + gamma_theta}  # sum of precisions
+	theta_hat = theta_var * {(N/sigma2)*ybar + mu_theta * gamma_theta}
 	theta = rnorm(1, theta_hat, sqrt(theta_var))
 	
 	# Conjugate update for sigma2, given theta
-	omega = rgamma(1, a + N/2, rate = b + sum((y - theta)^2)/2)
+	omega = rgamma(1, (d + N)/2, rate = {eta2 + sum((y - theta)^2)}/2)
 	sigma2 = 1/omega
 	
 	# Save draws
@@ -40,8 +40,8 @@ for(i in 1:NMC) {
 	sigma2_save[i] = sigma2
 }
 
-hist(theta_save, 20);  abline(v = theta_true, col='red', lwd=2)
-hist(sigma2_save, 20); abline(v = sig_true^2, col='red', lwd=2)
-
+# Posteriors
+hist(theta_save, 50, prob=TRUE);  abline(v = theta_true, col='blue', lwd=2)
+hist(sigma2_save, 50, prob=TRUE); abline(v = sig_true^2, col='red', lwd=2)
 
 
