f3d022392a15535421809f009d49aa5408868b16
James Scott <james.scott@mccombs.utexas.edu>
2017-03-29 16:16:01 -0500

    cheese lmer

--- r/cheese_lmer.R
+++ r/cheese_lmer.R
@@ -1,8 +1,17 @@
 library(lme4)
 library(ggplot2)
+library(lattice)
 
 cheese = read.csv("../data/cheese.csv")
 summary(cheese)
+
+boxplot(log(vol) ~ disp, data=cheese)
+
+# confounders?
+plot(log(vol) ~ log(price), data=cheese)
+boxplot(log(price) ~ disp, data=cheese)
+
+xyplot(log(vol) ~ log(price) | store, data=cheese)
 
 # common regression parameters across stores
 hlm1 = lmer(log(vol) ~ log(price) + disp + (1 | store), data=cheese)
@@ -16,10 +25,12 @@ coef(hlm2)
 
 
 # interactions store by store
-hlm3 = lmer(log(vol) ~ (log(price) + disp + log(price):disp | store), data=cheese)
+hlm3 = lmer(log(vol) ~ (1 + log(price) + disp + log(price):disp | store), data=cheese)
 summary(hlm3)
 coef(hlm3)
 
+lm_kroger = lm(log(vol) ~ log(price) + disp + log(price):disp,
+	data=subset(cheese, store == 'DALLAS/FT. WORTH - KROGER CO'))
 coef(hlm3)$store['DALLAS/FT. WORTH - KROGER CO',]
 
 plot(vol ~ price,
@@ -37,23 +48,6 @@ points(vol ~ price,
 curve(exp(9.933197)*x^(-1.800734), add=TRUE, col='blue')
 curve(exp(9.933197 + 0.9507379)*x^(-1.800734-0.623256), add=TRUE, col='red')
 
-
-
-
-plot(vol ~ price,
-	data=subset(cheese, store == 'DALLAS/FT. WORTH - KROGER CO'))
-points(vol ~ price,
-	pch=19, col='blue',
-	data=subset(cheese, store == 'DALLAS/FT. WORTH - KROGER CO'
-						& disp == 0))
-points(vol ~ price,
-	pch=19, col='red',
-	data=subset(cheese, store == 'DALLAS/FT. WORTH - KROGER CO'
-						& disp == 1))
-
-# Add curves				
-curve(exp(9.933197)*x^(-1.800734), add=TRUE, col='blue')
-curve(exp(9.933197 + 0.9507379)*x^(-1.800734-0.623256), add=TRUE, col='red')
 
 
 # What about for a store that didn't use displays often?
